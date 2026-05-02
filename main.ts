a7c054125021120d64e2a3c3e5f3c0a4b8f5d966
pizagama2-jpg <pizagama2@gmail.com>
2026-05-02 20:03:24 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,30 @@
+let 配列 = [300, 300]
+let zero = 0
+basic.forever(function () {
+    for (let vero of 配列) {
+        basic.showLeds(`
+            . # . . .
+            # # # # #
+            . # . . #
+            . . . . #
+            . . . # #
+            `)
+        pins.analogWritePin(AnalogPin.P16, vero)
+        basic.pause(5000)
+    }
+    pins.analogWritePin(AnalogPin.P16, zero)
+    basic.pause(1000)
+    for (let vero of 配列) {
+        basic.showLeds(`
+            . . . # .
+            # # # # #
+            # . . # .
+            # . . . .
+            # # . . .
+            `)
+        pins.analogWritePin(AnalogPin.P15, vero)
+        basic.pause(5000)
+    }
+    pins.analogWritePin(AnalogPin.P15, zero)
+    basic.pause(1000)
+})
